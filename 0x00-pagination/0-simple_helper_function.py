43d043475b24e3475b5016b8ae405267c61df7ec
Malika <malikaoubilla@gmail.com>
2024-05-20 13:26:36 +0100

    00

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from typing import Tuple
 """
-function file
+function file here
 """
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ function """
+    """ function that shows range """
     return (((page - 1) * page_size, page * page_size))
