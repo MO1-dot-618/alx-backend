6f6c2a4ece4dc4c82a0fa51a4c9c1338bd4b3bbb
Malika <malikaoubilla@gmail.com>
2024-05-20 15:13:42 +0100

    pycode

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,4 +52,3 @@ class Server:
         return {'page_size': page_size, 'page': page, 'data': data,
                 'next_page': next_page,
                 'prev_page': prev_page, 'total_pages': total_pages}
-    
