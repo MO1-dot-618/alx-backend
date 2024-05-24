e54ab76b937455e196c2f7c7e3389a3def9ed551
Malika <malikaoubilla@gmail.com>
2024-05-24 14:29:03 +0100

    2.2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -19,14 +19,12 @@ class LIFOCache(BaseCaching):
         """ Add an item in the cache
         """
         if (key and item):
-            self.cache_data[key] = item
-            self.order.append(key)
-
-            # If cache exceeds max items, discard the first item (FIFO)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.order.pop()  # Remove the oldest key
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = self.order.pop()
                 del self.cache_data[last_key]
                 print(f"DISCARD: {last_key}")
+            self.cache_data[key] = item
+            self.order.append(key)
 
     def get(self, key):
         """ Get an item by key
