dc8b4b3e8e88b71daaac0254e983b1f1b4f7dc4d
Malika <malikaoubilla@gmail.com>
2024-05-24 14:05:58 +0100

    1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.order = []  # List to keep track of the order of keys
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if(key and item):
+            self.cache_data[key] = item
+            self.order.append(key)
+            
+            # If the cache exceeds the maximum items, discard the first item (FIFO)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                oldest_key = self.order.pop(0)  # Remove the oldest key
+                del self.cache_data[oldest_key]  # Remove the oldest item from the cache
+                print(f"DISCARD: {oldest_key}")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
