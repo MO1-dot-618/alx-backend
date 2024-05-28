1ffd814a3600b4bbe4d9fe3c4caa6cf907e9dc43
Malika <malikaoubilla@gmail.com>
2024-05-28 17:22:36 +0100

    edit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
+def index():
     """Home"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
