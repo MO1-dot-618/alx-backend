29a84b32d361e764ccaf17b899f05b7159179fd1
Malika <malikaoubilla@gmail.com>
2024-05-28 17:45:43 +0100

    0-1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-""" Basic Flask app
-"""
+"""basic flask app"""
 
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Home"""
+    """ home """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Basic Flask app
-"""
+"""basic flask app"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -12,16 +11,18 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
 babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """Home"""
+    """ home """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
