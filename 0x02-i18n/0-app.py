043b7c4cc2ca278424bf5cf42c5a2b6eeca79da3
Malika <malikaoubilla@gmail.com>
2024-05-28 17:21:31 +0100

    flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Basic Flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Home"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
